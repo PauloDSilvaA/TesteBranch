391d070fdf73c052c148ffa11bc4a6881ad00b90
Paulo Diego <paulodsilva@utic.pe.senac.br>
2023-10-23 15:51:21 -0300

    Create Teste laco.py

---
+++ Teste laco.py
@@ -0,0 +1,10 @@
+lista = []
+for recebe in range(2):
+    nome = input("Nome :")
+    idade = int(input("Idade :"))
+    profi = input("Profissao :")
+    lista.append(nome)
+    lista.append(idade)
+    lista.append(profi)
+
+print(lista6554)
